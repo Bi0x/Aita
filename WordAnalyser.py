36484ddbb7e4d6b67b530b9dfbaf06f10ea86172
Bi0x <bi0x@foxmail.com>
2020-12-09 15:58:44 +0800

    Architecture updated

---
+++ WordAnalyser.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python3
+import csv
+import jieba, collections
+
+class WordAnalyser:
+
+    def read_remove_word(self):
+        # ? 获取需要剔除的字符和词语
+        fp = open('./assets/remove_words.csv', 'r')
+        csv_loader = csv.reader(fp)
+        return [eval(i).decode('utf-8') for i in list(csv_loader)[0]]
+
+    def create_remove_words(self):
+        # ? 添加需要剔除的关键词
+        remove_words = [
+            u'的',     u'，',   u'和',      u'是',      u'随着', 
+            u'对于',   u'对',   u'等',      u'能',      u'都', 
+            u'。',     u' ',    u'、',      u'中',      u'在', 
+            u'了',     u'通常', u'如果',    u'我们',    u'2',
+            u'需要',   u'\n',   u'.',       u'：',      u'，',
+            u',',      u'（',   u'）',      u'与',      u'有', 
+            u'会',     u'也',   u'以及',    u'可',      u'通过', 
+            u'上',     u'可以', u'并',      u"\u3000",  u'1',
+            u'to',     u'\t',   u'一个',    u'将',      u'到',
+            u'“',      u'”',    u'不',      u'地',      u'in',
+            u'于',     u'还',   u'我',      u'人',      u'为',
+            u'更',
+        ]
+        fp = open('./assets/remove_words.csv', 'w')
+        csv_saver = csv.writer(fp)
+        encode_remove_words = []
+        for i in remove_words:
+            encode_remove_words.append(i.encode('utf-8'))
+        csv_saver.writerow(encode_remove_words)
+        fp.close()
+
+    def keyword_analyse(self, answers):
+        # ? 关键词分析
+        remove_words = self.read_remove_word()
+        word_objects = []
+        for answer in answers:
+            word_list = jieba.cut(answer, cut_all=False)
+            for i in word_list:
+                if i not in remove_words:
+                    word_objects.append(i)
+        word_counts = collections.Counter(word_objects)
+        word_counts_top10 = word_counts.most_common(50)
+        print(word_counts_top10)
+
+if __name__ == '__main__':
+    word_analyser = WordAnalyser()
+    print("------->>> 目前剔除的词如下: <<<-------")
+    print(word_analyser.read_remove_word())
+    
--- main.py
+++ main.py
@@ -1,83 +1,72 @@
 #! /usr/bin/python3
 from sklearn.cluster import KMeans
-import jieba
 import re
-import collections
 
-def read_remove_word():
-    # ? 获取需要剔除的字符和词语
-    fp = open('./assets/remove_words.txt', 'r')
-    return ''.join(fp.readlines()).split('|')
-    
+from WordAnalyser import WordAnalyser
 
-def show_question_name():
-    # ? 显示问题
-    fp = open('./assets/question.txt')
-    print("针对的问题：《" + fp.readline()[:-1] + "》")
-    fp.close()
+# ! Trainer Definition
+class Trainer:
 
-
-def train_file_reader():
-    # ? 读取答案数据，并将答案分组
-    train_file = open('./assets/marked_answers.txt')
-    file_text = ""
-    for line in train_file.readlines():
-        file_text += line
-    regex_pattern = r'(\d{12}.*?#Total: *?\d\.*\d*/10)'
-    reg = re.compile(regex_pattern, re.DOTALL)
-    return reg.findall(file_text)
-
-
-def get_total_score(answers):
-    # ? 分析答案，获取评分
-    total_score = []
-    regex_pattern = r'#Total: *?(\d\.*\d*)/10'
-    reg = re.compile(regex_pattern, re.DOTALL)
-    for answer in answers:
-        total_score.append(float(reg.findall(answer)[0]))
-    return total_score
-
-
-def get_answers_main(answers):
-    # ? 获取主要回答内容
-    answers_main = []
-    regex_pattern = r':\d\d\n(.*)?#Marking Scheme'
-    reg = re.compile(regex_pattern, re.DOTALL)
-    for answer in answers:
-        answers_main.append(reg.findall(answer)[0])
-    return answers_main
-
-
-def get_answers_len(answers):
-    # ? 获取回答长度
-    answers_len = []
-    for answer in answers:
-        answers_len.append(len(answer))
-    return answers_len
-
-
-def keyword_analyse(answers):
-    # ? 关键词分析
-    remove_words = read_remove_word()
-    word_objects = []
-    for answer in answers:
-        word_list = jieba.cut(answer, cut_all=False)
-        for i in word_list:
-            if i not in remove_words:
-                word_objects.append(i)
-    word_counts = collections.Counter(word_objects)
-    word_counts_top10 = word_counts.most_common(50)
-    print(word_counts_top10)
-
-
-def train():
+    # ? answers_chunk   ->      每个人的回答与评分整体内容
+    # ? answers_score    ->      每个人的分数
+    # ? answers_main     ->      每个人的回答的文字内容
+    # ? answers_len      ->      每个人的回答的文字内容的长度
     # ? 训练因子选择：[字数, 关键词统计]
-    marked_answers = train_file_reader()
-    total_score = get_total_score(marked_answers)
-    main_answers = get_answers_main(marked_answers)
-    answers_len = get_answers_len(marked_answers)
-    keyword_analyse(main_answers)
+    def __init__(self):
+        super().__init__()
+        self.answers_chunk = self.train_file_reader()
+        self.answers_score = self.get_total_score(self.answers_chunk)
+        self.answers_main = self.get_answers_main(self.answers_chunk)
+        self.answers_len = self.get_answers_len(self.answers_chunk)
 
+    def show_question_name(self):
+        # ? 显示问题
+        fp = open('./assets/question.txt')
+        print("针对的问题：《" + fp.readline()[:-1] + "》")
+        fp.close()
+
+    def train_file_reader(self):
+        # ? 读取答案数据，并将答案分组
+        train_file = open('./assets/marked_answers.txt')
+        file_text = ""
+        for line in train_file.readlines():
+            file_text += line
+        regex_pattern = r'(\d{12}.*?#Total: *?\d\.*\d*/10)'
+        reg = re.compile(regex_pattern, re.DOTALL)
+        return reg.findall(file_text)
+
+    def get_total_score(self, answers):
+        # ? 分析答案，获取评分
+        total_score = []
+        regex_pattern = r'#Total: *?(\d\.*\d*)/10'
+        reg = re.compile(regex_pattern, re.DOTALL)
+        for answer in answers:
+            total_score.append(float(reg.findall(answer)[0]))
+        return total_score
+
+    def get_answers_main(self, answers):
+        # ? 获取主要回答内容
+        answers_main = []
+        regex_pattern = r':\d\d\n(.*)?#Marking Scheme'
+        reg = re.compile(regex_pattern, re.DOTALL)
+        for answer in answers:
+            answers_main.append(reg.findall(answer)[0])
+        return answers_main
+
+    def get_answers_len(self, answers):
+        # ? 获取回答长度
+        answers_len = []
+        for answer in answers:
+            answers_len.append(len(answer))
+        return answers_len
+
+
+# ! Trainer Definition End
+
+def train(self):
+    trainer = Trainer()
+
+    trainer.keyword_analyse(trainer.answers_main)
 
 if __name__ == '__main__':
     # show_question_name()
